c8ddc36aecf84232c74ce37eb565033e7ae0794e
Andrew Schwartz <andrewschwartz525@gmail.com>
2023-12-05 12:02:34 -0500

    landing page update

--- client/src/routes/Landing Page.tsx
+++ client/src/routes/Landing Page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Paper, TextField, Button, Typography, CssBaseline } from '@mui/material';
+import 'fontsource-inter/latin.css';  // Importing Inter font
 
 function LandingPage() {
   return (
@@ -13,9 +14,23 @@ function LandingPage() {
         bgcolor: 'background.default',
         color: 'text.primary',
         p: 3,
+        position: 'relative',
       }}
     >
       <CssBaseline />
+      <Typography
+        sx={{
+          position: 'absolute',
+          top: 30, // Adjust as needed
+          left: 50, // Adjust as needed
+          fontFamily: 'Inter, sans-serif',
+          fontSize: '1.75rem',
+          color: 'black',
+          m: 2, // Add margin for spacing
+        }}
+      >
+        Roadmap.ai
+      </Typography>
       <Paper
         elevation={0}
         sx={{
@@ -24,7 +39,7 @@ function LandingPage() {
           flexDirection: 'column',
           alignItems: 'center',
           width: '100%',
-          maxWidth: 'sm', // Adjust the maxWidth for smaller screens if needed
+          maxWidth: 480,
           mt: 4,
         }}
       >
@@ -35,11 +50,11 @@ function LandingPage() {
           align="center"
           sx={{
             fontWeight: 700,
-            fontSize: '2rem', // Reduced font size to fit in one line
-            maxWidth: '100%', // Ensure the text doesn't exceed the width of its container
+            fontSize: '2.4rem',
+            lineHeight: '0.3', // Add this line to ensure single line display
           }}
         >
-          Transition into Product
+          Transition Into Product
         </Typography>
         <Typography
           variant="subtitle1"
@@ -54,7 +69,7 @@ function LandingPage() {
           label="What do you currently do for work? Be as descriptive as possible!"
           margin="normal"
           multiline
-          minRows={2}
+          minRows={1}
           maxRows={4}
           sx={{
             '.MuiFilledInput-root': {
